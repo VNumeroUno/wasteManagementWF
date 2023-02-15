8993db4c20feede696e23f2b0870050e040a1de9
demo ConvergentIS <demo@convergentis.com>
2023-02-15 09:57:29 +0000

    Commit

--- workflow-ui-module/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["wasteManagement/workflowuimodule/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- workflow-ui-module/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"wasteManagement/workflow-ui-module/test/unit/controller/wasteManagement.controller"
-], function () {
-	"use strict";
-});
